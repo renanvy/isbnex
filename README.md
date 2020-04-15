baedf212766b15208d144cc80de2e4c04860f8cc
Renan Carneiro <renanvy@gmail.com>
2020-04-15 18:54:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ISBN
+
+A library to work with ISBN written in Elixir.
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `isbn` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:isbn, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/isbn](https://hexdocs.pm/isbn).
+
---
+++ mix.exs
@@ -0,0 +1,52 @@
+defmodule Isbn.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :isbn,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      description: "A library to work with ISBN written in Elixir.",
+      package: package(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      name: "ISBN",
+      docs: docs(),
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      name: "isbn",
+      maintainers: ["Renan Carneiro Oliveira"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/renanvy/isbn"}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp docs do
+    [
+      source_url: "https://github.com/renan/isbn",
+      extras: ["README.md"]
+    ]
+  end
+end
