307e4af5b73fb9c783c7b771196a0fb0e5268762
Renan Carneiro <renanvy@gmail.com>
2020-04-16 10:17:30 -0300

    ⬆️ Bump version isbnex to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Isbn.MixProject do
   def project do
     [
       app: :isbnex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
